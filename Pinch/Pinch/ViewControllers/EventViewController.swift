0b93761997e143a34f04cbd8390d069daf283454
Cameron Wu <cameron.wu@airbnb.com>
2015-10-18 16:21:02 -0700

    Onboarding Click-Through Hooked Up

--- Pinch/Pinch/ViewControllers/EventViewController.swift
+++ Pinch/Pinch/ViewControllers/EventViewController.swift
@@ -21,6 +21,9 @@ class EventViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didPressCloseButton(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
