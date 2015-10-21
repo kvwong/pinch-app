e1b443198a900c2e6aa18ab5a0a5a2a1c63dfffc
Kevin Wong <kdubz313@gmail.com>
2015-10-21 02:08:34 -0700

    added event detail UI

--- Pinch/Pinch/ViewControllers/EventViewController.swift
+++ Pinch/Pinch/ViewControllers/EventViewController.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class EventViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var eventSummaryView: UIView!
+    @IBOutlet weak var wireframeImage: UIImageView!
+    
+    @IBOutlet weak var eventBannerImage: UIImageView!
+    @IBOutlet weak var eventTitleLabel: UILabel!
+    @IBOutlet weak var eventTimeAndDateLabel: UILabel!
+    @IBOutlet weak var eventAddressLabel: UILabel!
+    
+    @IBOutlet weak var eventNPOLabel: UILabel!
+    
+    @IBOutlet weak var eventFriendCountLabel: UILabel!
+    @IBOutlet weak var eventDescriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        wireframeImage.alpha = 0.0
+        
+        scrollView.contentSize.height = eventSummaryView.frame.size.height
+        print(scrollView.contentSize.height)
+        print(eventSummaryView.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     @IBAction func didPressCloseButton(sender: UIButton) {
