853a86c989716dc9735fda4b20c97aa8a4a41494
Cameron Wu <cameronchwu@gmail.com>
2015-10-29 21:38:58 -0700

    Search Transitions!

--- Pinch/Pinch/ViewControllers/HomeViewController.swift
+++ Pinch/Pinch/ViewControllers/HomeViewController.swift
@@ -29,6 +29,8 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
     
     var isPresenting: Bool = true
     var isSearchEnabled: Bool = false
+    var searchTerm = ""
+    var searchLocation = ""
     
     @IBOutlet weak var tagButton1: UIButton!
     @IBOutlet weak var tagButton2: UIButton!
@@ -92,6 +94,9 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
     @IBAction func didPressFilterButton(sender: UIButton) {
         performSegueWithIdentifier("segueToFilters", sender: nil)
     }
+    
+    
+    // Event Cards -------------------------------------
 
     @IBAction func onTap(sender: AnyObject) {
         cardView = sender.view as UIView!
@@ -103,10 +108,8 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
         let translation = sender.translationInView(view)
         let velocity = sender.velocityInView(view)
         
-        
         if sender.state == UIGestureRecognizerState.Began {
             initialY = eventView.frame.origin.y
-            
         } else if sender.state == UIGestureRecognizerState.Changed {
             eventView.frame.origin.y = initialY + translation.y
             
@@ -117,17 +120,17 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
                 eventView.transform = CGAffineTransformMakeScale(multipleX, multipleY)
             }
             
-            
         } else if sender.state == UIGestureRecognizerState.Ended {
             if eventView.frame.origin.y > 10 {
                 eventView.frame.origin.y = initialY
-                
             } else if eventView.frame.origin.y < 10 {
                 eventView.frame.origin.y = 0.0
-                }
             }
+        }
             
     }
+    
+    
     // Custom Transitions ------------------------------
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -148,8 +151,8 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
             let destinationViewController = segue.destinationViewController as! SearchViewController
             destinationViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
             destinationViewController.transitioningDelegate = self
-            if searchTermField.text != "" {
-                destinationViewController.searchTerm = searchTermField.text! // Assumes search is never empty
+            if self.searchTermField.text != "" {
+                destinationViewController.searchTerm = self.searchTermField.text! // Assumes search is never empty
             }
         }
     }
@@ -160,12 +163,14 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
         if fromViewController.searchTermField.text! != "" {
             print("Search Term Field contains: \(fromViewController.searchTermField.text!)")
             isSearchEnabled = true
-            searchView.hidden = false
-            searchTermField.text = fromViewController.searchTermField.text!
+            self.searchView.hidden = false
+            self.searchTermField.text = fromViewController.searchTermField.text!
+            searchTerm = fromViewController.searchTermField.text!
+            searchLocation = fromViewController.searchLocationField.text!
         } else {
             print("Search Term Field is empty.")
             isSearchEnabled = false
-            searchView.hidden = true
+            self.searchView.hidden = true
         }
     }
     
@@ -186,7 +191,7 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
     }
     
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-        print("Animating transition...")
+        print("Animating transition to Search...")
         let containerView = transitionContext.containerView()!
         let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
         let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
@@ -194,41 +199,129 @@ class HomeViewController: UIViewController, UIViewControllerTransitioningDelegat
         let animationTime = 0.3
         
         if (isPresenting) {
-            containerView.addSubview(toViewController.view)
-            toViewController.view.alpha = 0
-            UIView.animateWithDuration(0.3, animations: { () -> Void in
-                toViewController.view.alpha = 1
-                }) { (finished: Bool) -> Void in
+            if toViewController.isKindOfClass(SearchViewController) { // Animate TO SearchViewController
+                containerView.addSubview(toViewController.view)
+                
+                if self.isSearchEnabled {
+                    print("Building `tempTextField`...")
+                    let window = UIApplication.sharedApplication().keyWindow
+                    let tempSearchTermField = UITextField(frame: CGRectMake(52, 28, 271, 28))
+                    let tempSearchLocationField = UITextField(frame: tempSearchTermField.frame) // Create them in the same place
+                    
+                    tempSearchTermField.layer.cornerRadius = buttonCornerRadius
+                    tempSearchTermField.leftView = UIImageView(image: UIImage(named: "icon_search_13x13"))
+                    tempSearchTermField.leftView?.contentMode = UIViewContentMode.Center
+                    tempSearchTermField.leftViewMode = UITextFieldViewMode.Always
+                    tempSearchTermField.attributedPlaceholder = NSAttributedString(string: "Search",
+                        attributes:[NSFontAttributeName: UIFont(name: "Lato-Regular", size: 14)!, NSForegroundColorAttributeName: UIColorFromRGB("FFFFFF", alpha: 0.5)])
+                    tempSearchTermField.attributedText = NSAttributedString(string: self.searchTerm, attributes: [NSFontAttributeName: UIFont(name: "Lato-Regular", size: 14)!, NSForegroundColorAttributeName:UIColor.whiteColor()])
+                    tempSearchTermField.leftView?.frame.size.width = 28
+                    tempSearchTermField.backgroundColor = UIColorFromRGB("389E8E")
+                    
+                    tempSearchLocationField.layer.cornerRadius = buttonCornerRadius
+                    tempSearchLocationField.leftView = UIImageView(image: UIImage(named: "icon_location_13x13"))
+                    tempSearchLocationField.leftView?.contentMode = UIViewContentMode.Center
+                    tempSearchLocationField.leftViewMode = UITextFieldViewMode.Always
+                    tempSearchLocationField.attributedPlaceholder = NSAttributedString(string: "Location",
+                        attributes:[NSFontAttributeName: UIFont(name: "Lato-Regular", size: 14)!, NSForegroundColorAttributeName: UIColorFromRGB("FFFFFF", alpha: 0.5)])
+                    tempSearchLocationField.attributedText = NSAttributedString(string: self.searchLocation, attributes: [NSFontAttributeName: UIFont(name: "Lato-Regular", size: 14)!, NSForegroundColorAttributeName:UIColor.whiteColor()])
+                    tempSearchLocationField.leftView?.frame.size.width = 28
+                    tempSearchLocationField.backgroundColor = UIColorFromRGB("389E8E")
+                    
+                    window!.addSubview(tempSearchLocationField)
+                    window!.addSubview(tempSearchTermField)
+                    
+                    let toVC = toViewController as! SearchViewController
+                    toVC.searchTermField.alpha = 0
+                    toVC.searchLocationField.alpha = 0
+                    toVC.view.backgroundColor = UIColorFromRGB(colorBrandGreenCode, alpha: 0)
+                    
                     transitionContext.completeTransition(true)
+                    
+                    UIView.animateWithDuration(animationTime, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: { () -> Void in
+                        print("Animating `tempTextField`...")
+                        tempSearchTermField.frame.origin = CGPoint(x: 16, y: 64)
+                        tempSearchTermField.frame.size.width = 343
+                        tempSearchLocationField.frame.origin = CGPoint(x: 16, y: 100)
+                        tempSearchLocationField.frame.size.width = 343
+                        }) { (finished: Bool) -> Void in
+                            tempSearchTermField.removeFromSuperview()
+                            tempSearchLocationField.removeFromSuperview()
+                            toVC.searchTermField.alpha = 1
+                            toVC.searchLocationField.alpha = 1
+                            toVC.view.backgroundColor = UIColorFromRGB(colorBrandGreenCode, alpha: 1)
+                    }
+                } else {
+                    toViewController.view.alpha = 0
+                    UIView.animateWithDuration(animationTime, animations: { () -> Void in
+                        toViewController.view.alpha = 1
+                        }) { (finished: Bool) -> Void in
+                            transitionContext.completeTransition(true)
+                    }
+                }
+                /*
+                let image = UIGraphicsGetImageFromCurrentImageContext()
+                let blurredImage = image.applyBlurWithRadius(
+                CGFloat(5),
+                tintColor: nil,
+                saturationDeltaFactor: 1.0,
+                maskImage: nil
+                )
+                */
+            } else {
+                // DO SOMETHING!!!
             }
-            if self.isSearchEnabled {
-                UIView.animateWithDuration(animationTime, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: { () -> Void in
-                    self.searchTermField.frame.origin = CGPoint(x: 16, y: 44)
-                    self.searchTermField.frame.size.width = 343
-                    }, completion: nil)
-            }
-            /*
-            let image = UIGraphicsGetImageFromCurrentImageContext()
-            let blurredImage = image.applyBlurWithRadius(
-            CGFloat(5),
-            tintColor: nil,
-            saturationDeltaFactor: 1.0,
-            maskImage: nil
-            )
-            */
         } else {
-            containerView.addSubview(searchTermField) // Not working yet
-            UIView.animateWithDuration(animationTime, animations: { () -> Void in
-                fromViewController.view.alpha = 0
-                }) { (finished: Bool) -> Void in
-                    transitionContext.completeTransition(true)
-                    fromViewController.view.removeFromSuperview()
-            }
-            if self.isSearchEnabled {
-                UIView.animateWithDuration(animationTime, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: { () -> Void in
-                    self.searchTermField.frame.origin = CGPoint(x: 52, y: 8)
-                    self.searchTermField.frame.size.width = 271
-                    }, completion: nil)
+            if fromViewController.isKindOfClass(SearchViewController) { // Animate FROM SearchViewController
+                let fromVC = fromViewController as! SearchViewController
+                fromVC.searchTermField.alpha = 0
+                fromVC.searchLocationField.alpha = 0
+                UIView.animateWithDuration(animationTime, animations: { () -> Void in
+                    fromViewController.view.alpha = 0
+                    }) { (finished: Bool) -> Void in
+                        transitionContext.completeTransition(true)
+                        fromViewController.view.removeFromSuperview()
+                }
+                if self.isSearchEnabled {
+                    print("Building `tempTextField`...")
+                    let window = UIApplication.sharedApplication().keyWindow
+                    let tempSearchTermField = UITextField(frame: CGRectMake(16, 64, 343, 28))
+                    let tempSearchLocationField = UITextField(frame: CGRectMake(16, 100, 343, 28))
+                    
+                    tempSearchTermField.layer.cornerRadius = buttonCornerRadius
+                    tempSearchTermField.leftView = UIImageView(image: UIImage(named: "icon_search_13x13"))
+                    tempSearchTermField.leftView?.contentMode = UIViewContentMode.Center
+                    tempSearchTermField.leftViewMode = UITextFieldViewMode.Always
+                    tempSearchTermField.attributedPlaceholder = NSAttributedString(string: "Search",
+                        attributes:[NSForegroundColorAttributeName: UIColorFromRGB("FFFFFF", alpha: 0.5)])
+                    tempSearchTermField.attributedText = NSAttributedString(string: self.searchTermField.text!, attributes: [NSFontAttributeName: UIFont(name: "Lato-Regular", size: 14)!, NSForegroundColorAttributeName:UIColor.whiteColor()])
+                    tempSearchTermField.leftView?.frame.size.width = 28
+                    tempSearchTermField.backgroundColor = UIColorFromRGB("389E8E")
+                    
+                    tempSearchLocationField.layer.cornerRadius = buttonCornerRadius
+                    tempSearchLocationField.leftView = UIImageView(image: UIImage(named: "icon_location_13x13"))
+                    tempSearchLocationField.leftView?.contentMode = UIViewContentMode.Center
+                    tempSearchLocationField.leftViewMode = UITextFieldViewMode.Always
+                    tempSearchLocationField.attributedPlaceholder = NSAttributedString(string: "Location",
+                        attributes:[NSForegroundColorAttributeName: UIColorFromRGB("FFFFFF", alpha: 0.5)])
+                    tempSearchLocationField.attributedText = NSAttributedString(string: "", attributes: [NSFontAttributeName: UIFont(name: "Lato-Regular", size: 14)!, NSForegroundColorAttributeName:UIColor.whiteColor()])
+                    tempSearchLocationField.leftView?.frame.size.width = 28
+                    tempSearchLocationField.backgroundColor = UIColorFromRGB("389E8E")
+                    
+                    window!.addSubview(tempSearchLocationField)
+                    window!.addSubview(tempSearchTermField)
+                    
+                    UIView.animateWithDuration(animationTime, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: { () -> Void in
+                        print("Animating `tempTextField`...")
+                        tempSearchTermField.frame.origin = CGPoint(x: 52, y: 28)
+                        tempSearchTermField.frame.size.width = 271
+                        tempSearchLocationField.frame.origin = CGPoint(x: 52, y: 28)
+                        tempSearchLocationField.frame.size.width = 271
+                        }) { (finished: Bool) -> Void in
+                            tempSearchTermField.removeFromSuperview()
+                            tempSearchLocationField.removeFromSuperview()
+                    }
+                }
             }
         }
     }
